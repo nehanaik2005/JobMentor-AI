a5a39e4a0fd457eda7469adf352ed8b26e0f2be8
neha <nehanaik12.nn@gmail.com>
2026-09-04 19:36:30 +0530

    Fix React frontend serving

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,18 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://jobmentor-ai.onrender.com"
+    ],
     credentials: true
 }))
 
@@ -15,11 +20,22 @@ app.use(cors({
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
 /* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
+/* Serve React frontend */
+const frontendPath = path.join(__dirname, "../../Frontend/dist")
 
+app.use(express.static(frontendPath))
+
+/* React fallback */
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        return res.sendFile(path.join(frontendPath, "index.html"))
+    }
+
+    next()
+})
 
 module.exports = app
