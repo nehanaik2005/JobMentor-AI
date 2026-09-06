0efaa5fd1febcdef0b70f257fd4a9b4313407570
neha <nehanaik12.nn@gmail.com>
2026-09-06 20:48:31 +0530

    commit

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,7 +1,7 @@
 const { GoogleGenAI } = require("@google/genai")
 const { z } = require("zod")
 const { zodToJsonSchema } = require("zod-to-json-schema")
-const generatePdfFromHtml = require("../utils/generatePdf")
+const generatePdfFromHtml = require("../utils/pdfGenerator")
 
 
 // ===============================
